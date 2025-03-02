1352a5029b3bbcc666c552ff72c3215165535bcf
Felipe Ortiz Alvarán <fortizalvaran@gmail.com>
2025-03-01 20:19:03 -0500

    metodos

---
+++ src/batalla_naval.py
@@ -0,0 +1,19 @@
+
+
+def iniciar_campo_juego(ancho, alto):
+    pass
+
+def disparar(fila, columna):
+    pass
+
+def iniciar_sesion(usuario, contraseña):
+    pass
+
+def crear_cuenta(usuario, contraseña):
+    pass
+
+def cambiar_contraseña(usuario, nueva_contraseña):
+    pass
+
+def visualizar_puntuaciones():
+    pass
